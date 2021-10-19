2be4fd7d0a01dee37d8c8f9e1ea54578ad1cd064
ZARIF ubuntu <uzbzarif@gmail.com>
2021-10-19 18:50:27 +0900

    closed #6

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 from torch.utils.data import dataset
 from utils.datasets import CatDogDataset,split_dataset,show_data,show_grid
 from pathlib import Path
-
+from models.model import CustomVGG11
 # Select device
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Doing computations on device = {}'.format(device))
@@ -27,7 +27,7 @@ print('Val:',len(test_data))
 
 #Iterate through the DataLoader
 
-show_grid(test_data,save=True)
+# show_grid(test_data,save=True)
 
 # Preparing your data for training with DataLoader
 
@@ -37,4 +37,10 @@ test_dataloader = DataLoader(test_data, batch_size=16, shuffle=True)
 # Visualizations single dataloader (Display image and label.)
 # Iterate through the DataLoader
 train_features, train_labels  = next(iter(train_dataloader))
-show_data((train_features[0], train_labels[0]),save=True)
+# show_data((train_features[0], train_labels[0]),save=True)
+
+
+model = CustomVGG11().to(device)
+x = torch.randn(2,3,224,224).to(device)
+y = model(x)
+print(y.size())
