3279baba470e3db9823cd93612873a9fad4e7fb8
ZARIF ubuntu <uzbzarif@gmail.com>
2021-10-19 15:19:25 +0900

    add argparse

---
+++ utils/check_image.py
@@ -0,0 +1,28 @@
+import os
+import argparse
+from pathlib import Path
+from PIL import Image
+def check_image(fn):
+    try:
+        im = Image.open(fn)
+        exif_data = im._getexif()
+        if im.mode!='RGB':
+          return False
+        im.verify()
+        return True
+    except:
+        return False
+    
+def check_image_dir(path):
+    for fn in path:
+        if not check_image(fn):
+            print("Corrupt image: {}".format(fn))
+            os.remove(fn)
+
+parser = argparse.ArgumentParser(description='DADASET DIR')
+parser.add_argument('--dir', default='data/PetImages', type=str, help='dataset folder')
+args = parser.parse_args()
+DATA_DIR = args.dir
+DATA_LIST = list(Path('data/PetImages/').glob('*/*.jpg'))
+#python utils/check_image.py --dir='data/PetImages'
+print(DATA_LIST[0])
